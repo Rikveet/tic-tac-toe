ad051c90913e49a3e343256b2a93d9cc5ba8cc18
CaptKillSwitch <62815232+CaptKillSwitch@users.noreply.github.com>
2022-11-04 01:12:40 -0400

    Possibly fixed missing stored usernames

--- src/Pages/Game/Components/Form/Form components/Input/InputField.tsx
+++ src/Pages/Game/Components/Form/Form components/Input/InputField.tsx
@@ -1,21 +1,14 @@
-import React, {ChangeEvent, useEffect, useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import "./InputField.css";
 import {IValidator} from "../../../../../../Types/Intefaces";
 
 const InputField = (props: { iptT: string, htmF: string, plh: string, name: string, labelText: string, validate: IValidator }) => {
     const [error, setError] = useState("");
     const [errorExpand, setErrorExpand] = useState(false);
-    const [storedValue, setStoredValue] = useState<string | undefined>();
+    let storedVal = window.localStorage.getItem(props.name);
+    const [storedValue, setStoredValue] = useState<string>(storedVal != undefined ? storedVal : '');
     const maxErrorVisibleChars = 10;
 
-    useEffect(() => {
-        return () => {
-            const storedVal = window.localStorage.getItem(props.name);
-            if (storedVal != undefined) {
-                setStoredValue(storedVal);
-            }
-        };
-    }, []);
 
     const handleFormChange = (e: ChangeEvent<HTMLInputElement>) => {
 
@@ -39,7 +32,7 @@ const InputField = (props: { iptT: string, htmF: string, plh: string, name: stri
             <label className={'inputFieldTip -mt-3 top-0 hover:scale-[1.4] hover:translate-y-1/4 '}
                    htmlFor={props.htmF}>{props.labelText}</label>
             <input className={'h-full w-full bg-transparent border-4 rounded-xl py-6 px-4 text-white border-solid'}
-                   type={props.iptT} placeholder={props.plh} name={props.name} value={storedValue && storedValue!=='' ? storedValue:''}
+                   type={props.iptT} placeholder={props.plh} name={props.name} value={storedValue && storedValue !== '' ? storedValue : ''}
                    onChange={handleFormChange}/>
             <span
                 className={
